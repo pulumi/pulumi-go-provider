badda48a115a8b60fa240d105ef720853fa9eaf6
Ian Wahbe <ian@wahbe.com>
2022-06-29 15:09:47 -0700

    Satisfy linter

--- resource/context.go
+++ resource/context.go
@@ -1,3 +1,17 @@
+// Copyright 2022, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package resource
 
 import (
@@ -18,7 +32,7 @@ type Context interface {
 	//
 	// For example:
 	// ```go
-	// func (r *MyResource) Update(ctx resource.Context, id string, newSalt any, ignoreChanges []string, preview bool) error {
+	// func (r *MyResource) Update(ctx resource.Context, _ string, newSalt any, _ []string, preview bool) error {
 	//     new := newSalt.(*RandomSalt)
 	//     if new.FieldInput != r.FieldInput {
 	//         ctx.MarkComputed(&r.ComputedField)        // This is valid
