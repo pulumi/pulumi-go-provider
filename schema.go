6d138ed5ef692fce3e3d95dd4a917bd0d6df106f
Harry <hliuson@hotmail.com>
2022-07-01 12:10:42 -0700

    tidying

--- schema.go
+++ schema.go
@@ -17,9 +17,9 @@ package provider
 import (
 	"encoding/json"
 	"fmt"
-	"os"
 	"reflect"
 
+	inputmap "github.com/pulumi/pulumi-go-provider/internal/inputmap"
 	"github.com/pulumi/pulumi-go-provider/internal/introspect"
 	"github.com/pulumi/pulumi-go-provider/types"
 	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
@@ -43,7 +43,7 @@ type serializationInfo struct {
 	resources map[reflect.Type]string
 	types     map[reflect.Type]string
 	enums     map[reflect.Type]string
-	inputMap  inputToImplementor
+	inputMap  inputmap.InputToImplementor
 }
 
 // Serialize a package to JSON Schema.
@@ -75,7 +75,7 @@ func serializeSchema(opts options) (schema.PackageSpec, error) {
 	info.resources = make(map[reflect.Type]string)
 	info.types = make(map[reflect.Type]string)
 	info.enums = make(map[reflect.Type]string)
-	info.inputMap = initializeInputMap()
+	info.inputMap = inputmap.GetInputMap()
 
 	for _, resource := range opts.Customs {
 		t := reflect.TypeOf(resource)
@@ -150,7 +150,7 @@ func serializeSchema(opts options) (schema.PackageSpec, error) {
 			spec.Types[token] = enumSpec
 			continue
 		}
-		typeSpec, err := serializeType(t, info)
+		typeSpec, err := serializeComplexType(t, info)
 		if err != nil {
 			return schema.PackageSpec{}, err
 		}
@@ -184,248 +184,6 @@ func serializeEnumType(enum types.Enum) (schema.ComplexTypeSpec, error) {
 	}
 
 	return spec, nil
-}
-
-func initializeInputMap() inputToImplementor {
-	var inputMap inputToImplementor = make(map[reflect.Type]reflect.Type)
-	//IntInput to int
-	inputMap.add((*pulumi.IntInput)(nil), (*int)(nil))
-
-	//IntPtrInput to *int
-	inputMap.add((*pulumi.IntPtrInput)(nil), (**int)(nil))
-
-	//IntArrayInput to []int
-	inputMap.add((*pulumi.IntArrayInput)(nil), (*[]int)(nil))
-
-	//IntMapInput to map[string]int
-	inputMap.add((*pulumi.IntMapInput)(nil), (*map[string]int)(nil))
-
-	//IntArrayMapInput to map[string][]int
-	inputMap.add((*pulumi.IntArrayMapInput)(nil), (*map[string][]int)(nil))
-
-	//IntMapArrayInput to []map[string]int
-	inputMap.add((*pulumi.IntMapArrayInput)(nil), (*[]map[string]int)(nil))
-
-	//IntMapMapInput to map[string]map[string]int
-	inputMap.add((*pulumi.IntMapMapInput)(nil), (*map[string]map[string]int)(nil))
-
-	//IntArrayArrayInput to [][]int
-	inputMap.add((*pulumi.IntArrayArrayInput)(nil), (*[][]int)(nil))
-
-	//StringInput to string
-	inputMap.add((*pulumi.StringInput)(nil), (*string)(nil))
-
-	//StringPtrInput to *string
-	inputMap.add((*pulumi.StringPtrInput)(nil), (**string)(nil))
-
-	//StringArrayInput to []string
-	inputMap.add((*pulumi.StringArrayInput)(nil), (*[]string)(nil))
-
-	//StringMapInput to map[string]string
-	inputMap.add((*pulumi.StringMapInput)(nil), (*map[string]string)(nil))
-
-	//StringArrayMapInput to map[string][]string
-	inputMap.add((*pulumi.StringArrayMapInput)(nil), (*map[string][]string)(nil))
-
-	//StringMapArrayInput to []map[string]string
-	inputMap.add((*pulumi.StringMapArrayInput)(nil), (*[]map[string]string)(nil))
-
-	//StringMapMapInput to map[string]map[string]string
-	inputMap.add((*pulumi.StringMapMapInput)(nil), (*map[string]map[string]string)(nil))
-
-	//StringArrayArrayInput to [][]string
-	inputMap.add((*pulumi.StringArrayArrayInput)(nil), (*[][]string)(nil))
-
-	//URNInput to pulumi.URN
-	inputMap.add((*pulumi.URNInput)(nil), (*pulumi.URN)(nil))
-
-	//URNPtrInput to *pulumi.URN
-	inputMap.add((*pulumi.URNPtrInput)(nil), (**pulumi.URN)(nil))
-
-	//URNArrayInput to []pulumi.URN
-	inputMap.add((*pulumi.URNArrayInput)(nil), (*[]pulumi.URN)(nil))
-
-	//URNMapInput to map[string]pulumi.URN
-	inputMap.add((*pulumi.URNMapInput)(nil), (*map[string]pulumi.URN)(nil))
-
-	//URNArrayMapInput to map[string][]pulumi.URN
-	inputMap.add((*pulumi.URNArrayMapInput)(nil), (*map[string][]pulumi.URN)(nil))
-
-	//URNMapArrayInput to []map[string]pulumi.URN
-	inputMap.add((*pulumi.URNMapArrayInput)(nil), (*[]map[string]pulumi.URN)(nil))
-
-	//URNMapMapInput to map[string]map[string]pulumi.URN
-	inputMap.add((*pulumi.URNMapMapInput)(nil), (*map[string]map[string]pulumi.URN)(nil))
-
-	//URNArrayArrayInput to [][]pulumi.URN
-	inputMap.add((*pulumi.URNArrayArrayInput)(nil), (*[][]pulumi.URN)(nil))
-
-	//ArchiveInput to pulumi.Archive
-	inputMap.add((*pulumi.ArchiveInput)(nil), (*pulumi.Archive)(nil))
-
-	//ArchiveArrayInput to []pulumi.Archive
-	inputMap.add((*pulumi.ArchiveArrayInput)(nil), (*[]pulumi.Archive)(nil))
-
-	//ArchiveMapInput to map[string]pulumi.Archive
-	inputMap.add((*pulumi.ArchiveMapInput)(nil), (*map[string]pulumi.Archive)(nil))
-
-	//ArchiveArrayMapInput to map[string][]pulumi.Archive
-	inputMap.add((*pulumi.ArchiveArrayMapInput)(nil), (*map[string][]pulumi.Archive)(nil))
-
-	//ArchiveMapArrayInput to []map[string]pulumi.Archive
-	inputMap.add((*pulumi.ArchiveMapArrayInput)(nil), (*[]map[string]pulumi.Archive)(nil))
-
-	//ArchiveMapMapInput to map[string]map[string]pulumi.Archive
-	inputMap.add((*pulumi.ArchiveMapMapInput)(nil), (*map[string]map[string]pulumi.Archive)(nil))
-
-	//ArchiveArrayArrayInput to [][]pulumi.Archive
-	inputMap.add((*pulumi.ArchiveArrayArrayInput)(nil), (*[][]pulumi.Archive)(nil))
-
-	//AssetInput to pulumi.Asset
-	inputMap.add((*pulumi.AssetInput)(nil), (*pulumi.Asset)(nil))
-
-	//AssetArrayInput to []pulumi.Asset
-	inputMap.add((*pulumi.AssetArrayInput)(nil), (*[]pulumi.Asset)(nil))
-
-	//AssetMapInput to map[string]pulumi.Asset
-	inputMap.add((*pulumi.AssetMapInput)(nil), (*map[string]pulumi.Asset)(nil))
-
-	//AssetArrayMapInput to map[string][]pulumi.Asset
-	inputMap.add((*pulumi.AssetArrayMapInput)(nil), (*map[string][]pulumi.Asset)(nil))
-
-	//AssetMapArrayInput to []map[string]pulumi.Asset
-	inputMap.add((*pulumi.AssetMapArrayInput)(nil), (*[]map[string]pulumi.Asset)(nil))
-
-	//AssetMapMapInput to map[string]map[string]pulumi.Asset
-	inputMap.add((*pulumi.AssetMapMapInput)(nil), (*map[string]map[string]pulumi.Asset)(nil))
-
-	//AssetArrayArrayInput to [][]pulumi.Asset
-	inputMap.add((*pulumi.AssetArrayArrayInput)(nil), (*[][]pulumi.Asset)(nil))
-
-	//AssetOrArchiveInput to pulumi.AssetOrArchive
-	inputMap.add((*pulumi.AssetOrArchiveInput)(nil), (*pulumi.AssetOrArchive)(nil))
-
-	//AssetOrArchiveArrayInput to []pulumi.AssetOrArchive
-	inputMap.add((*pulumi.AssetOrArchiveArrayInput)(nil), (*[]pulumi.AssetOrArchive)(nil))
-
-	//AssetOrArchiveMapInput to map[string]pulumi.AssetOrArchive
-	inputMap.add((*pulumi.AssetOrArchiveMapInput)(nil), (*map[string]pulumi.AssetOrArchive)(nil))
-
-	//AssetOrArchiveArrayMapInput to map[string][]pulumi.AssetOrArchive
-	inputMap.add((*pulumi.AssetOrArchiveArrayMapInput)(nil), (*map[string][]pulumi.AssetOrArchive)(nil))
-
-	//AssetOrArchiveMapArrayInput to []map[string]pulumi.AssetOrArchive
-	inputMap.add((*pulumi.AssetOrArchiveMapArrayInput)(nil), (*[]map[string]pulumi.AssetOrArchive)(nil))
-
-	//AssetOrArchiveMapMapInput to map[string]map[string]pulumi.AssetOrArchive
-	inputMap.add((*pulumi.AssetOrArchiveMapMapInput)(nil), (*map[string]map[string]pulumi.AssetOrArchive)(nil))
-
-	//AssetOrArchiveArrayArrayInput to [][]pulumi.AssetOrArchive
-	inputMap.add((*pulumi.AssetOrArchiveArrayArrayInput)(nil), (*[][]pulumi.AssetOrArchive)(nil))
-
-	//BoolInput to bool
-	inputMap.add((*pulumi.BoolInput)(nil), (*bool)(nil))
-
-	//BoolArrayInput to []bool
-	inputMap.add((*pulumi.BoolArrayInput)(nil), (*[]bool)(nil))
-
-	//BoolMapInput to map[string]bool
-	inputMap.add((*pulumi.BoolMapInput)(nil), (*map[string]bool)(nil))
-
-	//BoolArrayMapInput to map[string][]bool
-	inputMap.add((*pulumi.BoolArrayMapInput)(nil), (*map[string][]bool)(nil))
-
-	//BoolMapArrayInput to []map[string]bool
-	inputMap.add((*pulumi.BoolMapArrayInput)(nil), (*[]map[string]bool)(nil))
-
-	//BoolMapMapInput to map[string]map[string]bool
-	inputMap.add((*pulumi.BoolMapMapInput)(nil), (*map[string]map[string]bool)(nil))
-
-	//BoolArrayArrayInput to [][]bool
-	inputMap.add((*pulumi.BoolArrayArrayInput)(nil), (*[][]bool)(nil))
-
-	//IDInput to pulumi.ID
-	inputMap.add((*pulumi.IDInput)(nil), (*pulumi.ID)(nil))
-
-	//IDPtrInput to *pulumi.ID
-	inputMap.add((*pulumi.IDPtrInput)(nil), (**pulumi.ID)(nil))
-
-	//IDArrayInput to []pulumi.ID
-	inputMap.add((*pulumi.IDArrayInput)(nil), (*[]pulumi.ID)(nil))
-
-	//IDMapInput to map[string]pulumi.ID
-	inputMap.add((*pulumi.IDMapInput)(nil), (*map[string]pulumi.ID)(nil))
-
-	//IDArrayMapInput to map[string][]pulumi.ID
-	inputMap.add((*pulumi.IDArrayMapInput)(nil), (*map[string][]pulumi.ID)(nil))
-
-	//IDMapArrayInput to []map[string]pulumi.ID
-	inputMap.add((*pulumi.IDMapArrayInput)(nil), (*[]map[string]pulumi.ID)(nil))
-
-	//IDMapMapInput to map[string]map[string]pulumi.ID
-	inputMap.add((*pulumi.IDMapMapInput)(nil), (*map[string]map[string]pulumi.ID)(nil))
-
-	//IDArrayArrayInput to [][]pulumi.ID
-	inputMap.add((*pulumi.IDArrayArrayInput)(nil), (*[][]pulumi.ID)(nil))
-
-	//ArrayInput to []interface{}
-	inputMap.add((*pulumi.ArrayInput)(nil), (*[]interface{})(nil))
-
-	//MapInput to map[string]interface{}
-	inputMap.add((*pulumi.MapInput)(nil), (*map[string]interface{})(nil))
-
-	//ArrayMapInput to map[string][]interface{}
-	inputMap.add((*pulumi.ArrayMapInput)(nil), (*map[string][]interface{})(nil))
-
-	//MapArrayInput to []map[string]interface{}
-	inputMap.add((*pulumi.MapArrayInput)(nil), (*[]map[string]interface{})(nil))
-
-	//MapMapInput to map[string]map[string]interface{}
-	inputMap.add((*pulumi.MapMapInput)(nil), (*map[string]map[string]interface{})(nil))
-
-	//ArrayArrayInput to [][]interface{}
-	inputMap.add((*pulumi.ArrayArrayInput)(nil), (*[][]interface{})(nil))
-
-	//ArrayArrayMapInput to map[string][][]interface{}
-	inputMap.add((*pulumi.ArrayArrayMapInput)(nil), (*map[string][][]interface{})(nil))
-
-	//Float65Input to float64
-	inputMap.add((*pulumi.Float64Input)(nil), (*float64)(nil))
-
-	//Float64PtrInput to *float64
-	inputMap.add((*pulumi.Float64PtrInput)(nil), (**float64)(nil))
-
-	//Float64ArrayInput to []float64
-	inputMap.add((*pulumi.Float64ArrayInput)(nil), (*[]float64)(nil))
-
-	//Float64MapInput to map[string]float64
-	inputMap.add((*pulumi.Float64MapInput)(nil), (*map[string]float64)(nil))
-
-	//Float64ArrayMapInput to map[string][]float64
-	inputMap.add((*pulumi.Float64ArrayMapInput)(nil), (*map[string][]float64)(nil))
-
-	//Float64MapArrayInput to []map[string]float64
-	inputMap.add((*pulumi.Float64MapArrayInput)(nil), (*[]map[string]float64)(nil))
-
-	//Float64MapMapInput to map[string]map[string]float64
-	inputMap.add((*pulumi.Float64MapMapInput)(nil), (*map[string]map[string]float64)(nil))
-
-	//Float64ArrayArrayInput to [][]float64
-	inputMap.add((*pulumi.Float64ArrayArrayInput)(nil), (*[][]float64)(nil))
-
-	//ResourceInput to pulumi.Resource
-	inputMap.add((*pulumi.ResourceInput)(nil), (*pulumi.Resource)(nil))
-
-	//ResourceArrayInput to []pulumi.Resource
-	inputMap.add((*pulumi.ResourceArrayInput)(nil), (*[]pulumi.Resource)(nil))
-
-	return inputMap
-}
-
-type inputToImplementor map[reflect.Type]reflect.Type
-
-func (m inputToImplementor) add(k interface{}, v interface{}) {
-	m[reflect.TypeOf(k).Elem()] = reflect.TypeOf(v).Elem()
 }
 
 func mergePackageSpec(spec, over schema.PackageSpec) (schema.PackageSpec, error) {
@@ -554,7 +312,7 @@ func mergeObjectTypeSpec(base, over schema.ObjectTypeSpec) schema.ObjectTypeSpec
 	return base
 }
 
-// Get the resourceSpec for a single resource
+// Get the ResourceSpec for a single resource
 func serializeResource(rawResource interface{}, info serializationInfo) (schema.ResourceSpec, error) {
 	v := reflect.ValueOf(rawResource)
 	for v.Type().Kind() == reflect.Ptr {
@@ -621,12 +379,12 @@ func serializeResource(rawResource interface{}, info serializationInfo) (schema.
 	return spec, nil
 }
 
-//Get the propertySpec for a single property
+//Get the PropertySpec for a single property
 func serializeProperty(t reflect.Type, description string, info serializationInfo) (schema.PropertySpec, error) {
 	t = dereference(t)
 	typeKind, enum := getTypeKind(t)
 	if enum {
-		enumSpec, err := serializeEnum(t, info)
+		enumSpec, err := serializeTypeEnum(t, info)
 		if err != nil {
 			return schema.PropertySpec{}, err
 		}
@@ -635,7 +393,7 @@ func serializeProperty(t reflect.Type, description string, info serializationInf
 			Description: description,
 		}, nil
 	} else if isTypeOrResource(t, info) {
-		typeSpec, err := serializeRef(t, info)
+		typeSpec, err := serializeReferenceType(t, info)
 		if err != nil {
 			return schema.PropertySpec{}, err
 		}
@@ -651,7 +409,7 @@ func serializeProperty(t reflect.Type, description string, info serializationInf
 
 	switch typeKind {
 	case ARRAY:
-		itemSpec, err := serializeTypeRef(t.Elem(), info)
+		itemSpec, err := serializeTypeAny(t.Elem(), info)
 		if err != nil {
 			return schema.PropertySpec{}, err
 		}
@@ -663,7 +421,7 @@ func serializeProperty(t reflect.Type, description string, info serializationInf
 			},
 		}, nil
 	case OBJECT:
-		valSpec, err := serializeTypeRef(t.Elem(), info)
+		valSpec, err := serializeTypeAny(t.Elem(), info)
 		if err != nil {
 			return schema.PropertySpec{}, err
 		}
@@ -687,7 +445,8 @@ func serializeProperty(t reflect.Type, description string, info serializationInf
 	}
 }
 
-func serializeRef(t reflect.Type, info serializationInfo) (*schema.TypeSpec, error) {
+//Get a TypeSpec which is a reference.
+func serializeReferenceType(t reflect.Type, info serializationInfo) (*schema.TypeSpec, error) {
 	t = dereference(t)
 	if token, ok := info.resources[t]; ok {
 		return &schema.TypeSpec{
@@ -700,7 +459,6 @@ func serializeRef(t reflect.Type, info serializationInfo) (*schema.TypeSpec, err
 		}, nil
 	}
 	return nil, fmt.Errorf("unknown reference type %s", t)
-
 }
 
 func isTypeOrResource(t reflect.Type, info serializationInfo) bool {
@@ -713,7 +471,6 @@ func isTypeOrResource(t reflect.Type, info serializationInfo) bool {
 	return isType
 }
 
-//Get the typeSpec for a single type
 func getTypeKind(t reflect.Type) (string, bool) {
 	var typeName string
 	isEnum := false
@@ -738,7 +495,7 @@ func getTypeKind(t reflect.Type) (string, bool) {
 		//The user should not be able to cause this to happen
 		//I removed the behavior where getTypeKind would automatically dereference pointers
 		//Because it may be confusing when debugging if getTypeKind is returning non-pointer
-		//When t is actually a pointe
+		//When t is actually a pointer
 		panic("Detected pointer type during serialization - did you forget to dereference?")
 	case reflect.Interface:
 		typeName = ANY
@@ -748,20 +505,20 @@ func getTypeKind(t reflect.Type) (string, bool) {
 	return typeName, isEnum
 }
 
-func serializeTypeRef(t reflect.Type, info serializationInfo) (*schema.TypeSpec, error) {
+func serializeTypeAny(t reflect.Type, info serializationInfo) (*schema.TypeSpec, error) {
 	typeKind, enum := getTypeKind(t)
 	if enum {
-		enumSpec, err := serializeEnum(t, info)
+		enumSpec, err := serializeTypeEnum(t, info)
 		if err != nil {
 			return nil, err
 		}
 		return &enumSpec, nil
 	} else if isTypeOrResource(t, info) {
-		return serializeRef(t, info)
+		return serializeReferenceType(t, info)
 	}
 	switch typeKind {
 	case ARRAY:
-		itemSpec, err := serializeTypeRef(t.Elem(), info)
+		itemSpec, err := serializeTypeAny(t.Elem(), info)
 		if err != nil {
 			return nil, err
 		}
@@ -770,7 +527,7 @@ func serializeTypeRef(t reflect.Type, info serializationInfo) (*schema.TypeSpec,
 			Items: itemSpec,
 		}, nil
 	case OBJECT:
-		valSpec, err := serializeTypeRef(t.Elem(), info)
+		valSpec, err := serializeTypeAny(t.Elem(), info)
 		if err != nil {
 			return nil, err
 		}
@@ -794,10 +551,10 @@ func serializeTypeRef(t reflect.Type, info serializationInfo) (*schema.TypeSpec,
 	}
 }
 
-func serializeType(typ interface{}, info serializationInfo) (schema.ComplexTypeSpec, error) {
+func serializeComplexType(typ interface{}, info serializationInfo) (schema.ComplexTypeSpec, error) {
 	t := reflect.TypeOf(typ)
 	t = dereference(t)
-	typeKind, enum := getTypeKind(t)
+	_, enum := getTypeKind(t)
 	if enum {
 		return schema.ComplexTypeSpec{}, fmt.Errorf("enums are implemented using provider.Enums()")
 	}
@@ -831,20 +588,10 @@ func serializeType(typ interface{}, info serializationInfo) (schema.ComplexTypeS
 		}, nil
 	}
 
-	enumVals := make([]schema.EnumValueSpec, 0)
-
-	fmt.Fprintf(os.Stderr, "overwrite the autogenerated type %s with your own: "+
-		"enum types must be manually specified", t)
-
-	return schema.ComplexTypeSpec{
-		ObjectTypeSpec: schema.ObjectTypeSpec{
-			Type: typeKind,
-		},
-		Enum: enumVals,
-	}, nil
+	return schema.ComplexTypeSpec{}, fmt.Errorf("Types must be structs - use provider.Enum to create an enum type %s", t)
 }
 
-func serializeEnum(t reflect.Type, info serializationInfo) (schema.TypeSpec, error) {
+func serializeTypeEnum(t reflect.Type, info serializationInfo) (schema.TypeSpec, error) {
 	t = dereference(t)
 	enum, ok := info.enums[t]
 	if !ok {
