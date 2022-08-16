c7b86dfe735788edc4fc5f2e2d603a3204deaf84
Ian Wahbe <ian@wahbe.com>
2022-08-16 15:01:30 +0200

    Add timeout test + lint

--- integration/tests/cancel_test.go
+++ integration/tests/cancel_test.go
@@ -1,3 +1,17 @@
+// Copyright 2022, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package cancel_test
 
 import (
@@ -13,8 +27,9 @@ import (
 )
 
 func TestGlobalCancel(t *testing.T) {
+	t.Parallel()
 	wg := new(sync.WaitGroup)
-	wg.Add(3)
+	wg.Add(4)
 	s := integration.NewServer("cancel", semver.MustParse("1.2.3"),
 		cancel.Wrap(&middleware.Scaffold{
 			CreateFn: func(ctx p.Context, req p.CreateRequest) (p.CreateResponse, error) {
@@ -33,5 +48,32 @@ func TestGlobalCancel(t *testing.T) {
 	go func() { _, err := s.Create(p.CreateRequest{}); assert.NoError(t, err) }()
 	assert.NoError(t, s.Cancel())
 	go func() { _, err := s.Create(p.CreateRequest{}); assert.NoError(t, err) }()
+	wg.Wait()
+}
+
+func TestTimeoutApplication(t *testing.T) {
+	t.Parallel()
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	s := integration.NewServer("cancel", semver.MustParse("1.2.3"),
+		cancel.Wrap(&middleware.Scaffold{
+			CreateFn: func(ctx p.Context, req p.CreateRequest) (p.CreateResponse, error) {
+				select {
+				case <-ctx.Done():
+					wg.Done()
+					return p.CreateResponse{
+						ID:         "cancled",
+						Properties: req.Properties,
+					}, nil
+				}
+			},
+		}))
+
+	go func() {
+		_, err := s.Create(p.CreateRequest{
+			Timeout: 0.5,
+		})
+		assert.NoError(t, err)
+	}()
 	wg.Wait()
 }
