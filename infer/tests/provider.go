68b2897d71e7698cafb5c4f692b6f97bd8412b90
Ian Wahbe <ian@wahbe.com>
2023-08-22 10:45:47 -0700

    Make outputs computed when appropriate (#105)
    * Fix outputs
    
    An output is marked as computed if:
    1. Any of its inputs are computed.
    2. Any of its inputs have changed.
    3. It is a preview before a create.
    
    This property can always be overridden by setting .Known() to true.
    
    We don't allow users to always set an input as computed because it will cause constant
    diffs.
    
    * Update existing tests
    
    * Add tests for Create
    
    * Add tests for update (default deps)
    
    * Add tests for update (explicit deps)
    
    * Add test for update (explicit with old inputs)

---
+++ infer/tests/provider.go
@@ -0,0 +1,170 @@
+// Copyright 2022, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tests
+
+import (
+	"fmt"
+
+	"github.com/blang/semver"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
+
+	p "github.com/pulumi/pulumi-go-provider"
+	"github.com/pulumi/pulumi-go-provider/infer"
+	"github.com/pulumi/pulumi-go-provider/integration"
+)
+
+func urn(typ, name string) resource.URN {
+	return resource.NewURN("stack", "proj", "",
+		tokens.Type("test:index:"+typ), tokens.QName(name))
+}
+
+type Echo struct{}
+type EchoInputs struct {
+	String string            `pulumi:"string"`
+	Int    int               `pulumi:"int"`
+	Map    map[string]string `pulumi:"strMap,optional"`
+}
+type EchoOutputs struct {
+	EchoInputs
+	Name      string            `pulumi:"nameOut"`
+	StringOut string            `pulumi:"stringOut"`
+	IntOut    int               `pulumi:"intOut"`
+	MapOut    map[string]string `pulumi:"strMapOut,optional"`
+}
+
+func (*Echo) Create(ctx p.Context, name string, inputs EchoInputs, preview bool) (string, EchoOutputs, error) {
+	id := name + "-id"
+	state := EchoOutputs{EchoInputs: inputs}
+	if preview {
+		return id, state, nil
+	}
+	state.Name = name
+	state.StringOut = inputs.String
+	state.IntOut = inputs.Int
+	state.MapOut = inputs.Map
+	return id, state, nil
+}
+
+func (*Echo) Update(ctx p.Context, id string, olds EchoOutputs, news EchoInputs, preview bool) (EchoOutputs, error) {
+	if preview {
+		return olds, nil
+	}
+
+	return EchoOutputs{
+		EchoInputs: news,
+		Name:       olds.Name,
+		StringOut:  news.String,
+		IntOut:     news.Int,
+		MapOut:     news.Map,
+	}, nil
+}
+
+var _ = (infer.ExplicitDependencies[WiredInputs, WiredOutputs])((*Wired)(nil))
+
+type Wired struct{}
+type WiredInputs struct {
+	String string `pulumi:"string"`
+	Int    int    `pulumi:"int"`
+}
+type WiredOutputs struct {
+	Name         string `pulumi:"name"`
+	StringAndInt string `pulumi:"stringAndInt"`
+	StringPlus   string `pulumi:"stringPlus"`
+}
+
+func (*Wired) Create(ctx p.Context, name string, inputs WiredInputs, preview bool) (string, WiredOutputs, error) {
+	id := name + "-id"
+	state := WiredOutputs{Name: "(" + name + ")"}
+	if preview {
+		return id, state, nil
+	}
+	state.StringPlus = inputs.String + "+"
+	state.StringAndInt = fmt.Sprintf("%s-%d", inputs.String, inputs.Int)
+	return id, state, nil
+}
+
+func (*Wired) Update(
+	ctx p.Context, id string, olds WiredOutputs, news WiredInputs, preview bool,
+) (WiredOutputs, error) {
+	return WiredOutputs{
+		Name:         id,
+		StringAndInt: fmt.Sprintf("%s-%d", news.String, news.Int),
+		StringPlus:   news.String + "++",
+	}, nil
+}
+
+func (*Wired) WireDependencies(f infer.FieldSelector, a *WiredInputs, s *WiredOutputs) {
+	stringIn := f.InputField(&a.String)
+	intIn := f.InputField(&a.Int)
+
+	name := f.OutputField(&s.Name)
+	stringAndInt := f.OutputField(&s.StringAndInt)
+	stringOut := f.OutputField(&s.StringPlus)
+
+	name.AlwaysKnown()            // This is based on the pulumi name, which is always known
+	stringOut.DependsOn(stringIn) // Passthrough value with a mutation
+	stringAndInt.DependsOn(stringIn)
+	stringAndInt.DependsOn(intIn)
+
+}
+
+var _ = (infer.ExplicitDependencies[WiredInputs, WiredOutputs])((*Wired)(nil))
+
+// Wired plus is like wired, but has its inputs embedded with its outputs.
+//
+// This allows it to remember old inputs when calculating which fields have changed.
+type WiredPlus struct{}
+type WiredPlusOutputs struct {
+	WiredInputs
+	WiredOutputs
+}
+
+func (*WiredPlus) Create(ctx p.Context, name string, inputs WiredInputs, preview bool) (string, WiredPlusOutputs, error) {
+	r := new(Wired)
+	id, out, err := r.Create(ctx, name, inputs, preview)
+	return id, WiredPlusOutputs{
+		WiredInputs:  inputs,
+		WiredOutputs: out,
+	}, err
+}
+
+func (*WiredPlus) Update(
+	ctx p.Context, id string, olds WiredPlusOutputs, news WiredInputs, preview bool,
+) (WiredPlusOutputs, error) {
+	r := new(Wired)
+	out, err := r.Update(ctx, id, olds.WiredOutputs, news, preview)
+	return WiredPlusOutputs{
+		WiredInputs:  news,
+		WiredOutputs: out,
+	}, err
+}
+
+func (*WiredPlus) WireDependencies(f infer.FieldSelector, a *WiredInputs, s *WiredPlusOutputs) {
+	r := new(Wired)
+	r.WireDependencies(f, a, &s.WiredOutputs)
+}
+
+func provider() integration.Server {
+	p := infer.Provider(infer.Options{
+		Resources: []infer.InferredResource{
+			infer.Resource[*Echo, EchoInputs, EchoOutputs](),
+			infer.Resource[*Wired, WiredInputs, WiredOutputs](),
+			infer.Resource[*WiredPlus, WiredInputs, WiredPlusOutputs](),
+		},
+		ModuleMap: map[tokens.ModuleName]tokens.ModuleName{"tests": "index"},
+	})
+	return integration.NewServer("test", semver.MustParse("1.0.0"), p)
+}
---
+++ infer/tests/update_test.go
@@ -0,0 +1,228 @@
+// Copyright 2022, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tests
+
+import (
+	"testing"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
+	"github.com/stretchr/testify/assert"
+
+	p "github.com/pulumi/pulumi-go-provider"
+)
+
+func TestUpdateManualDeps(t *testing.T) {
+	type m = resource.PropertyMap
+	c := resource.MakeComputed
+	s := resource.NewStringProperty
+	n := resource.NewNumberProperty
+
+	test := func(
+		testName, resource string,
+		olds, newsPreview, newsUpdate, expectedPreview, expectedUp m,
+	) {
+		t.Run(testName, func(t *testing.T) {
+			t.Run("preview", func(t *testing.T) {
+				prov := provider()
+				resp, err := prov.Update(p.UpdateRequest{
+					ID:   "some-id",
+					Urn:  urn(resource, "test"),
+					Olds: olds, News: newsPreview,
+					Preview: true,
+				})
+				assert.NoError(t, err)
+				assert.Equal(t, p.UpdateResponse{
+					Properties: expectedPreview,
+				}, resp)
+			})
+			t.Run("update", func(t *testing.T) {
+				prov := provider()
+				resp, err := prov.Update(p.UpdateRequest{
+					ID:   "some-id",
+					Urn:  urn(resource, "test"),
+					Olds: olds, News: newsUpdate,
+				})
+				assert.NoError(t, err)
+				assert.Equal(t, p.UpdateResponse{
+					Properties: expectedUp,
+				}, resp)
+			})
+		})
+	}
+
+	test("unchanged", "Wired",
+		m{"name": s("some-id"), "stringAndInt": s("str-5"), "stringPlus": s("str+")},  // Old Input
+		m{"string": s("str"), "int": n(5)},                                            // New Preview
+		m{"string": s("str"), "int": n(5)},                                            // New Update
+		m{"name": s("some-id"), "stringAndInt": s("str-5"), "stringPlus": s("str++")}, // Preview inputs
+		m{"name": s("some-id"), "stringAndInt": s("str-5"), "stringPlus": s("str++")}) // Full inputs
+
+	test("int-computed", "Wired",
+		m{"name": s("some-id"), "stringAndInt": s("str-5"), "stringPlus": s("str+")},     // Old Input
+		m{"string": s("str"), "int": c(n(5))},                                            // New Input
+		m{"string": s("str"), "int": n(10)},                                              // New Update
+		m{"name": s("some-id"), "stringAndInt": c(s("str-5")), "stringPlus": s("str++")}, // Preview inputs
+		m{"name": s("some-id"), "stringAndInt": s("str-10"), "stringPlus": s("str++")})   // Full inputs
+
+	test("string-computed", "Wired",
+		m{"name": s("some-id"), "stringAndInt": s("str-5"), "stringPlus": s("str+")},        // Old Input
+		m{"string": c(s("str")), "int": n(5)},                                               // New Input
+		m{"string": s("foo"), "int": n(5)},                                                  // New Update
+		m{"name": s("some-id"), "stringAndInt": c(s("str-5")), "stringPlus": c(s("str++"))}, // Preview inputs
+		m{"name": s("some-id"), "stringAndInt": s("foo-5"), "stringPlus": s("foo++")})       // Full inputs
+
+	test("int-changed", "WiredPlus",
+		m{ // Old Input
+			"name": s("some-id"), "stringAndInt": s("str-5"), "stringPlus": s("str+"),
+			"string": s("str"), "int": n(5),
+		},
+		m{"string": s("str"), "int": n(10)}, // New Input
+		m{"string": s("str"), "int": n(10)}, // New Update
+		m{ // Preview inputs: int changed -> stringAndInt is now computed
+			"name": s("some-id"), "stringAndInt": c(s("str-10")), "stringPlus": s("str++"),
+			"string": s("str"), "int": n(10),
+		},
+		m{ // Full inputs
+			"name": s("some-id"), "stringAndInt": s("str-10"), "stringPlus": s("str++"),
+			"string": s("str"), "int": n(10),
+		})
+
+	test("string-changed", "WiredPlus",
+		m{ // Old Input
+			"name": s("some-id"), "stringAndInt": s("str-5"), "stringPlus": s("str+"),
+			"string": s("old-str"), "int": n(5),
+		},
+		m{"string": s("new-str"), "int": n(5)}, // New Input
+		m{"string": s("new-str"), "int": n(5)}, // New Update
+		m{ // Preview inputs
+			"name": s("some-id"), "stringAndInt": c(s("new-str-5")), "stringPlus": c(s("new-str++")),
+			"string": s("new-str"), "int": n(5),
+		},
+		m{ // Full inputs
+			"name": s("some-id"), "stringAndInt": s("new-str-5"), "stringPlus": s("new-str++"),
+			"string": s("new-str"), "int": n(5),
+		})
+}
+
+func TestUpdateDefaultDeps(t *testing.T) {
+	c := resource.MakeComputed
+	s := resource.NewStringProperty
+	n := resource.NewNumberProperty
+
+	test := func(t *testing.T, newString resource.PropertyValue,
+		expectedPreview, expectedUp resource.PropertyMap) {
+		t.Run("preview", func(t *testing.T) {
+			prov := provider()
+			resp, err := prov.Update(p.UpdateRequest{
+				ID:  "some-id",
+				Urn: urn("Echo", "preview"),
+				Olds: resource.PropertyMap{
+					"string":    s("old-string"),
+					"int":       n(1),
+					"intOut":    n(1),
+					"nameOut":   s("old-name"),
+					"stringOut": s("old-string"),
+				},
+				News: resource.PropertyMap{
+					// Became computed
+					"string": newString,
+					"int":    n(1),
+				},
+				Preview: true,
+			})
+			assert.NoError(t, err)
+			assert.Equal(t, p.UpdateResponse{
+				Properties: expectedPreview,
+			}, resp)
+		})
+		t.Run("update", func(t *testing.T) {
+			prov := provider()
+			resp, err := prov.Update(p.UpdateRequest{
+				ID:  "some-id",
+				Urn: urn("Echo", "update"),
+				Olds: resource.PropertyMap{
+					"string":    s("old-string"),
+					"int":       n(1),
+					"intOut":    n(1),
+					"nameOut":   s("old-name"),
+					"stringOut": s("old-string"),
+				},
+				News: resource.PropertyMap{
+					// Became computed
+					"string": newString,
+					"int":    n(1),
+				},
+			})
+			assert.NoError(t, err)
+			assert.Equal(t, p.UpdateResponse{
+				Properties: expectedUp,
+			}, resp)
+		})
+	}
+	t.Run("computed", func(t *testing.T) {
+		test(t, c(s("old-string")),
+			resource.PropertyMap{
+				"string":    c(s("old-string")),
+				"int":       n(1),
+				"stringOut": c(s("old-string")),
+				"intOut":    c(n(1)),
+				"nameOut":   c(s("old-name")),
+			},
+			resource.PropertyMap{
+				"string":    s("old-string"),
+				"int":       n(1),
+				"stringOut": s("old-string"),
+				"intOut":    n(1),
+				"nameOut":   s("old-name"),
+			},
+		)
+	})
+	t.Run("changed", func(t *testing.T) {
+		test(t, s("new-string"),
+			resource.PropertyMap{
+				"string":    c(s("old-string")),
+				"int":       n(1),
+				"stringOut": c(s("old-string")),
+				"intOut":    c(n(1)),
+				"nameOut":   c(s("old-name")),
+			},
+			resource.PropertyMap{
+				"string":    s("new-string"),
+				"int":       n(1),
+				"stringOut": s("new-string"),
+				"intOut":    n(1),
+				"nameOut":   s("old-name"),
+			},
+		)
+	})
+	t.Run("unchanged", func(t *testing.T) {
+		test(t, s("old-string"),
+			resource.PropertyMap{
+				"string":    s("old-string"),
+				"int":       n(1),
+				"stringOut": s("old-string"),
+				"intOut":    n(1),
+				"nameOut":   s("old-name"),
+			},
+			resource.PropertyMap{
+				"string":    s("old-string"),
+				"int":       n(1),
+				"stringOut": s("old-string"),
+				"intOut":    n(1),
+				"nameOut":   s("old-name"),
+			},
+		)
+	})
+}
