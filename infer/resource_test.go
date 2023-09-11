ea49c5f9e9b12c7c11f3550b524fefd03b4932d0
Ian Wahbe <ian@wahbe.com>
2023-09-11 14:56:30 -0700

    Separate computed and secret dependencies (#123)
    Fixes #122 
    
    ---
    
    This PR separates dependency flow for computedness and secretness.
    
    By default, we apply the following strategies:
    
    ### Secrets
    An output property is secret if and only if there is an input property
    of the same name that is also secret
    
    ### Computed
    An output property is computed if there is any change in between old and
    new inputs unless there is an input property of the same name and value
    that is not computed.

--- infer/resource_test.go
+++ infer/resource_test.go
@@ -16,14 +16,113 @@ package infer
 
 import (
 	"context"
+	"reflect"
 	"testing"
 
-	provider "github.com/pulumi/pulumi-go-provider"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/diag"
 	r "github.com/pulumi/pulumi/sdk/v3/go/common/resource"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"pgregory.net/rapid"
+
+	provider "github.com/pulumi/pulumi-go-provider"
+	"github.com/pulumi/pulumi-go-provider/infer/internal/ende"
+	rRapid "github.com/pulumi/pulumi-go-provider/internal/rapid/resource"
 )
+
+// testGetDependencies runs a property test that asserts on the flow between inputs and
+// outputs for some I, O ∈ GoType and ∀ (old,new) ∈ I, out ∈ O.
+func testGetDependencies[I any, O any](t *testing.T,
+	wire func(FieldSelector, *I, *O),
+	assert func(t *testing.T, oldInput, newInput, output r.PropertyMap),
+) {
+	var i I
+	var o O
+	wireDeps := func(f FieldSelector) {
+		if wire != nil {
+			wire(f, &i, &o)
+		}
+	}
+	setDeps, err := getDependenciesRaw(
+		&i, &o, wireDeps,
+		false, /*isCreate*/
+		true /*isPreview*/)
+	require.NoError(t, err)
+
+	inputT := rapid.Just(reflect.TypeOf(i))
+	outputT := rapid.Just(reflect.TypeOf(o))
+
+	getMap := func(t rRapid.Typed) r.PropertyMap {
+		return t.Value.ObjectValue()
+	}
+
+	rapid.Check(t, func(rt *rapid.T) {
+		oldInput := rapid.Map(rRapid.ValueOf(inputT), getMap).
+			Draw(rt, "oldInput")
+		newInput := rapid.Map(rRapid.ValueOf(inputT), getMap).
+			Draw(rt, "newInput")
+		output := rapid.Map(rRapid.ValueOf(outputT), getMap).
+			Draw(rt, "output")
+
+		setDeps(oldInput, newInput, output)
+
+		assert(t, oldInput, newInput, output)
+	})
+}
+
+func TestDefaultDependencies(t *testing.T) {
+	t.Parallel()
+	type input struct {
+		I1 string            `pulumi:"i1"`
+		I2 *int              `pulumi:"i2,optional"`
+		I3 map[string]string `pulumi:"i3"`
+	}
+
+	type output struct {
+		input
+
+		O1 *string        `pulumi:"o1,optional"`
+		O2 float64        `pulumi:"o2"`
+		O3 map[string]int `pulumi:"o2"`
+	}
+
+	assert := func(t *testing.T, oldInput, newInput, output r.PropertyMap) {
+		if newInput.ContainsUnknowns() {
+			for k, v := range output {
+				if newV, ok := newInput[k]; ok &&
+					ende.DeepEquals(newV, v) {
+					continue
+				}
+				assert.True(t, ende.IsComputed(v),
+					"key: %q", string(k))
+			}
+		} else if !ende.DeepEquals(
+			r.NewObjectProperty(oldInput),
+			r.NewObjectProperty(newInput)) {
+			// If there is a change, then every item item should be
+			// computed, except items that mirror a known input.
+			for k, v := range output {
+				newV, ok := newInput[k]
+				if !ok {
+					assert.True(t, ende.IsComputed(v),
+						"key: %q", string(k))
+				} else if !ende.IsComputed(v) {
+					assert.True(t, ende.DeepEquals(v, newV))
+				}
+			}
+		}
+
+		for k, v := range output {
+			// An input of the same name is secret, so this should be too.
+			if newInput[k].ContainsSecrets() {
+				assert.Truef(t, ende.IsSecret(v),
+					"key: %q", string(k))
+			}
+		}
+	}
+
+	testGetDependencies[input, output](t, nil, assert)
+}
 
 func TestFieldGenerator(t *testing.T) {
 	t.Parallel()
