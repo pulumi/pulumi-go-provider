e7e843c318e42eb2cb9527ac49692428b0d3888c
Bryce Lampe <bryce@pulumi.com>
2024-04-26 09:20:52 -0700

    Update integration/integration.go
    Co-authored-by: Ian Wahbe <ian@wahbe.com>

--- integration/integration.go
+++ integration/integration.go
@@ -60,7 +60,7 @@ type server struct {
 	context context.Context
 }
 
-func (s *server) ctx(_ presource.URN) context.Context {
+func (s *server) ctx(presource.URN) context.Context {
 	return context.WithValue(s.context, key.RuntimeInfo, s.runInfo)
 }
 
