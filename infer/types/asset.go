81648d028aa49ef13eb2b8a3bac1b4fc0d331595
Thomas Kappler <tkappler@pulumi.com>
2024-06-11 10:31:16 +0200

    Document AssetOrArchive

--- infer/types/asset.go
+++ infer/types/asset.go
@@ -19,6 +19,10 @@ import "github.com/pulumi/pulumi/sdk/v3/go/common/resource"
 const AssetSignature = "a9e28acb8ab501f883219e7c9f624fb6"
 const ArchiveSignature = "195f3948f6769324d4661e1e245f3a4d"
 
+// AssetOrArchive is a union type that can represent either an Asset or an Archive.
+// Setting both fields to non-nil values is an error.
+// This type exists to accomodate the semantics of the core Pulumi SDK's Asset type,
+// which is also a union of Asset and Archive.
 type AssetOrArchive struct {
 	Asset   *resource.Asset   `pulumi:"a9e28acb8ab501f883219e7c9f624fb6,optional"`
 	Archive *resource.Archive `pulumi:"195f3948f6769324d4661e1e245f3a4d,optional"`
