90a53fc49250ebb6c9bf6451fef63c1e0471cc50
Ramon Quitales <ramon@pulumi.com>
2025-03-25 13:02:31 -0700

    add examples

---
+++ examples/component-provider/randomComponent.go
@@ -0,0 +1,54 @@
+// Copyright 2016-2025, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	p "github.com/pulumi/pulumi-go-provider"
+	"github.com/pulumi/pulumi-random/sdk/v4/go/random"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+type RandomComponent struct {
+	pulumi.ResourceState
+	RandomComponentArgs
+	Password        pulumi.StringOutput `pulumi:"password"`
+	HardcodedOutput pulumi.StringOutput `pulumi:"hardcodedOutput"`
+}
+
+type RandomComponentArgs struct {
+	Length pulumi.IntInput `pulumi:"length"`
+}
+
+func NewMyComponent(ctx *pulumi.Context, name string, compArgs RandomComponentArgs, opts ...pulumi.ResourceOption) (*RandomComponent, error) {
+	comp := &RandomComponent{}
+	err := ctx.RegisterComponentResource(p.GetTypeToken(ctx.Context()), name, comp, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	pArgs := &random.RandomPasswordArgs{
+		Length: compArgs.Length,
+	}
+
+	password, err := random.NewRandomPassword(ctx, name+"-password", pArgs, pulumi.Parent(comp))
+	if err != nil {
+		return nil, err
+	}
+
+	comp.Password = password.Result
+	comp.HardcodedOutput = pulumi.String("This is a hardcoded output string").ToStringOutput()
+
+	return comp, nil
+}
