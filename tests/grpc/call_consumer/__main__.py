fd2dee946e5bddf152ea5dd38801cd3a7dea613a
Eron Wright <eron@pulumi.com>
2025-04-15 17:17:08 -0700

    call_consumer exports for visibility

--- tests/grpc/call_consumer/__main__.py
+++ tests/grpc/call_consumer/__main__.py
@@ -4,6 +4,7 @@ import pulumi
 import pulumi_test as test
 
 c = test.Component(resource_name="my-component", my_input="foo")
+pulumi.export("my_output", c.my_output)
 
 result = c.my_method(arg1="bar")
 pulumi.export("resp1", result.resp1)
