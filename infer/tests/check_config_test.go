pulumi/pulumi-go-provider

commit f910c33e184d97f3c14bc2b7ee0a9c1108537112

Store pulumi-go-provider version in provider state (#298)
This PR adds the following output values to the provider resource
created with this framework:

- `__pulumi-go-provider-infer: true` indicates that [infer] was used to
create the provider
- `__pulumi-go-provider-version: <version>`identifies the version of
this `pulumi-go-provider` used to create the provider

Closes: #292

infer/configuration.go
@@ -20,6 +20,7 @@ import (
 	"reflect"
 
 	pschema "github.com/pulumi/pulumi/pkg/v3/codegen/schema"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/mapper"
 
@@ -78,6 +79,11 @@ func (*config[T]) GetSchema(reg schema.RegisterDerivativeType) (pschema.Resource
 	return r, errs.ErrorOrNil()
 }
 
+// markAsInferProvider adds a key to the provider state to indicate that [infer] is being used for this provider.
+func markAsInferProvider(pm resource.PropertyMap) {
+	pm[inferStateKeyName] = resource.NewBoolProperty(true)
+}
+
 func (c *config[T]) checkConfig(ctx context.Context, req p.CheckRequest) (p.CheckResponse, error) {
 	var t T
 	if v := reflect.ValueOf(t); v.Kind() == reflect.Pointer && v.IsNil() {
@@ -106,6 +112,9 @@ func (c *config[T]) checkConfig(ctx context.Context, req p.CheckRequest) (p.Chec
 		if err != nil {
 			return p.CheckResponse{}, err
 		}
+
+		markAsInferProvider(inputs)
+
 		return p.CheckResponse{
 			Inputs:   applySecrets[T](inputs),
 			Failures: failures,
@@ -126,6 +135,8 @@ func (c *config[T]) checkConfig(ctx context.Context, req p.CheckRequest) (p.Chec
 	if err != nil {
 		return p.CheckResponse{}, err
 	}
+
+	markAsInferProvider(news)
 
 	return p.CheckResponse{
 		Inputs:   applySecrets[T](news),

infer/provider.go
@@ -31,6 +31,8 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+const inferStateKeyName = "__pulumi-go-provider-infer"
+
 type configKeyType struct{}
 
 var configKey configKeyType

provider.go
@@ -654,6 +654,7 @@ func (p *provider) CheckConfig(ctx context.Context, req *rpc.CheckRequest) (*rpc
 	if err != nil {
 		return nil, err
 	}
+
 	r, err := p.client.CheckConfig(ctx, CheckRequest{
 		Urn:        presource.URN(req.GetUrn()),
 		Olds:       olds,
@@ -664,6 +665,9 @@ func (p *provider) CheckConfig(ctx context.Context, req *rpc.CheckRequest) (*rpc
 	if err != nil {
 		return nil, err
 	}
+
+	// Inject the version of pulumi-go-provider into the news map to store in state.
+	r.Inputs[frameworkStateKeyName] = presource.NewStringProperty(frameworkVersion.String())
 
 	inputs, err := p.asStruct(r.Inputs)
 	if err != nil {

version.go
@@ -1,0 +1,40 @@
+// Copyright 2016-2025, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package provider
+
+import (
+	_ "embed"
+	"strings"
+
+	"github.com/blang/semver"
+)
+
+const (
+	// frameworkStateKeyName is the key name used to store the framework version in
+	// a program's state.
+	frameworkStateKeyName = "__pulumi-go-provider-version"
+)
+
+//go:embed .version
+var versionStr string
+
+// Version is the framework's release version. Be aware that if the framework is installed
+// from a git commit, this will refer to the next proposed release, not a
+// format like "1.2.3-$COMMIT".
+var frameworkVersion semver.Version
+
+func init() {
+	frameworkVersion = semver.MustParse(strings.TrimSpace(versionStr))
+}

infer/tests/check_config_test.go
@@ -42,7 +42,8 @@ func TestCheckConfig(t *testing.T) {
 	// By default, check simply ensures that we can decode cleanly. It removes unknown
 	// fields so that diff doesn't trigger on changes to unwatched arguments.
 	assert.Equal(t, pMap{
-		"value": pString("foo"),
+		"value":                      pString("foo"),
+		"__pulumi-go-provider-infer": resource.NewBoolProperty(true),
 	}, resp.Inputs)
 }
 
@@ -64,17 +65,22 @@ func TestCheckConfigCustom(t *testing.T) {
 		assert.Equal(t, expected, resp.Inputs)
 	}
 
-	t.Run("empty", func(t *testing.T) { t.Parallel(); test(t, nil, pMap{}) })
+	t.Run("empty", func(t *testing.T) {
+		t.Parallel()
+		test(t, nil, pMap{
+			"__pulumi-go-provider-infer": resource.NewBoolProperty(true)})
+	})
 	t.Run("unknown", func(t *testing.T) {
 		t.Parallel()
 		test(t,
 			pMap{"unknownField": pString("bar")},
-			pMap{})
+			pMap{"__pulumi-go-provider-infer": resource.NewBoolProperty(true)})
 	})
 	t.Run("number", func(t *testing.T) {
 		t.Parallel()
 		test(t,
 			pMap{"number": pNumber(42)},
-			pMap{"number": pNumber(42.5)})
+			pMap{"number": pNumber(42.5),
+				"__pulumi-go-provider-infer": resource.NewBoolProperty(true)})
 	})
 }

tests/config_test.go
@@ -131,7 +131,8 @@ func TestInferCheckConfigSecrets(t *testing.T) {
 	require.NoError(t, err)
 	require.Empty(t, resp.Failures)
 	assert.Equal(t, resource.PropertyMap{
-		"field": resource.MakeSecret(resource.NewProperty("value")),
+		"__pulumi-go-provider-infer": resource.NewBoolProperty(true),
+		"field":                      resource.MakeSecret(resource.NewProperty("value")),
 		"nested": resource.NewProperty(resource.PropertyMap{
 			"int":        resource.MakeSecret(resource.NewProperty(1.0)),
 			"not-nested": resource.NewProperty("not-secret"),
@@ -208,9 +209,10 @@ func TestInferCustomCheckConfig(t *testing.T) {
 			require.NoError(t, err)
 			require.Empty(t, resp.Failures)
 			assert.Equal(t, resource.PropertyMap{
-				"field":         resource.MakeSecret(resource.NewProperty("value")),
-				"not":           resource.NewProperty("not-secret"),
-				"applyDefaults": resource.NewProperty(applyDefaults),
+				"__pulumi-go-provider-infer": resource.NewBoolProperty(true),
+				"field":                      resource.MakeSecret(resource.NewProperty("value")),
+				"not":                        resource.NewProperty("not-secret"),
+				"applyDefaults":              resource.NewProperty(applyDefaults),
 			}, resp.Inputs)
 		})
 	}

tests/grpc/config_test.go
@@ -15,6 +15,10 @@
 package grpc
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	replay "github.com/pulumi/providertest/replay"
@@ -24,6 +28,25 @@ import (
 	config "github.com/pulumi/pulumi-go-provider/tests/grpc/config/provider"
 )
 
+// injectFrameworkVersion replaces the {{VERSION}} placeholder in the log with the
+// version of the provider obtained from the repository's root .version file.
+func injectFrameworkVersion(logTemplate string) string {
+	// Get the absolute path to the .version file.
+	path, err := filepath.Abs("../../.version")
+	if err != nil {
+		panic(fmt.Errorf("unable to get absolute path: %w", err))
+	}
+
+	// Read the contents of the .version file.
+	version, err := os.ReadFile(path)
+	if err != nil {
+		panic(fmt.Errorf("unable to read .version file: %w", err))
+	}
+
+	// Replace the placeholder "{{VERSION}}" with the actual version.
+	return strings.ReplaceAll(logTemplate, "{{VERSION}}", string(version))
+}
+
 // These inputs were created by running `pulumi up` with PULUMI_DEBUG_GRPC=logs.json in
 // ./config/consumer.
 
@@ -32,7 +55,7 @@ import (
 // These test values were derived from a Pulumi YAML program, which does not JSON encode
 // it's values.
 func TestBasicConfig(t *testing.T) {
-	sequence := `[
+	sequence := injectFrameworkVersion(`[
   {
     "method": "/pulumirpc.ResourceProvider/CheckConfig",
     "request": {
@@ -91,6 +114,8 @@ func TestBasicConfig(t *testing.T) {
     },
     "response": {
       "inputs": {
+        "__pulumi-go-provider-infer": true,
+        "__pulumi-go-provider-version": "{{VERSION}}",
         "a": [
           "one",
           "two"
@@ -249,12 +274,12 @@ func TestBasicConfig(t *testing.T) {
       "name": "config"
     }
   }
-]`
+]`)
 	replayConfig(t, sequence)
 }
 
 func TestConfigWithSecrets(t *testing.T) {
-	sequence := `[
+	sequence := injectFrameworkVersion(`[
   {
     "method": "/pulumirpc.ResourceProvider/CheckConfig",
     "request": {
@@ -313,6 +338,8 @@ func TestConfigWithSecrets(t *testing.T) {
     },
     "response": {
       "inputs": {
+        "__pulumi-go-provider-infer": true,
+        "__pulumi-go-provider-version": "{{VERSION}}",
         "a": [
           "one",
           "two"
@@ -471,7 +498,7 @@ func TestConfigWithSecrets(t *testing.T) {
       "name": "config"
     }
   }
-]`
+]`)
 	replayConfig(t, sequence)
 }
 
@@ -519,7 +546,7 @@ func TestConfigWithSecrets(t *testing.T) {
 //
 // Write the program and run `consumer/run.sh` to generate gRPC logs.
 func TestJSONEncodedConfig(t *testing.T) {
-	replayConfig(t, `[{
+	replayConfig(t, injectFrameworkVersion(`[{
     "method": "/pulumirpc.ResourceProvider/CheckConfig",
     "request": {
         "urn": "urn:pulumi:test::test::pulumi:providers:config::ts",
@@ -539,6 +566,8 @@ func TestJSONEncodedConfig(t *testing.T) {
     },
     "response": {
         "inputs": {
+            "__pulumi-go-provider-infer": true,
+            "__pulumi-go-provider-version": "{{VERSION}}",
             "a": [
                 "one",
                 "two"
@@ -571,7 +600,7 @@ func TestJSONEncodedConfig(t *testing.T) {
         "mode": "client",
         "name": "config"
     }
-}]`)
+}]`))
 }
 
 func replayConfig(t *testing.T, jsonLog string) {

.github/workflows/release.yml
@@ -1,0 +1,50 @@
+name: Release
+
+on:
+  push:
+    tags:
+      - v*.*.*
+      - "!v*.*.*-**"
+
+jobs:
+  ensure-version:
+    runs-on: ubuntu-latest
+    name: Ensure .version file is up to date
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v2
+      - name: Ensure version
+        run: |
+          if [ ! -f ".version" ]; then
+            echo "No .version file found"
+            exit 1
+          fi
+          if [ "v$(cat .version)" != ${{ github.ref_name }} ]; then
+            echo 'The release has failed.'
+            echo
+            echo The version in ".version" file "(v$(cat .version))" does 
+            echo not match tag pushed (${{ github.ref_name }}). The releaser
+            echo needs to manually delete the release and any associated git
+            echo tag, update the .version file and try again.
+            exit 1
+          fi
+  lint:
+    uses: ./.github/workflows/stage-lint.yml
+  test:
+    uses: ./.github/workflows/stage-test.yml
+    with:
+      live-test: true
+    secrets:
+      PULUMI_ACCESS_TOKEN: ${{ secrets.PULUMI_ACCESS_TOKEN }}
+      CODECOV_TOKEN: ${{ secrets.CODECOV_TOKEN }}
+  publish:
+    needs: [ensure-version, lint, test]
+    runs-on: ubuntu-latest
+    steps:
+    - name: Create GH Release
+      uses: softprops/action-gh-release@c95fe1489396fe8a9eb87c0abf8aa5b2ef267fda # v2
+      with:
+            generate_release_notes: true
+      env:
+            GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+    

commit 239453950805f8eef55cd120b370110448d75203

Keep internal metadata under __internal (#377)
The `__internal` object is automatically excluded from diffs by the
engine as of https://github.com/pulumi/pulumi/pull/18440.

Bumping pu/pu to pull in that change resolves #288 for `__internal`
properties, but the issue still exists for the additional metadata
introduced in #298.

Rather than special-case the new metadata, let's move it into the
internal object.

infer/configuration.go
@@ -81,7 +81,15 @@ func (*config[T]) GetSchema(reg schema.RegisterDerivativeType) (pschema.Resource
 
 // markAsInferProvider adds a key to the provider state to indicate that [infer] is being used for this provider.
 func markAsInferProvider(pm resource.PropertyMap) {
-	pm[inferStateKeyName] = resource.NewBoolProperty(true)
+	internal := pm["__internal"]
+	if !internal.IsObject() {
+		newMap := resource.PropertyMap{}
+		internal = resource.NewObjectProperty(newMap)
+		pm["__internal"] = internal
+	}
+
+	m := internal.ObjectValue()
+	m[inferStateKeyName] = resource.NewBoolProperty(true)
 }
 
 func (c *config[T]) checkConfig(ctx context.Context, req p.CheckRequest) (p.CheckResponse, error) {

infer/provider.go
@@ -31,7 +31,7 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-const inferStateKeyName = "__pulumi-go-provider-infer"
+const inferStateKeyName = "pulumi-go-provider-infer"
 
 type configKeyType struct{}
 

provider.go
@@ -706,8 +706,15 @@ func (p *provider) CheckConfig(ctx context.Context, req *rpc.CheckRequest) (*rpc
 		return nil, err
 	}
 
+	internal, ok := r.Inputs.GetOk("__internal")
+	if !ok {
+		internal = property.New(property.NewMap(nil))
+	}
+	m := internal.AsMap()
+	m = m.Set(frameworkStateKeyName, property.New(frameworkVersion.String()))
+
 	// Inject the version of pulumi-go-provider into the news map to store in state.
-	r.Inputs = r.Inputs.Set(frameworkStateKeyName, property.New(frameworkVersion.String()))
+	r.Inputs = r.Inputs.Set("__internal", property.New(m))
 
 	inputs, err := p.asStruct(r.Inputs)
 	if err != nil {

version.go
@@ -24,7 +24,7 @@ import (
 const (
 	// frameworkStateKeyName is the key name used to store the framework version in
 	// a program's state.
-	frameworkStateKeyName = "__pulumi-go-provider-version"
+	frameworkStateKeyName = "pulumi-go-provider-version"
 )
 
 //go:embed .version

infer/tests/check_config_test.go
@@ -40,8 +40,10 @@ func TestCheckConfig(t *testing.T) {
 	// By default, check simply ensures that we can decode cleanly. It removes unknown
 	// fields so that diff doesn't trigger on changes to unwatched arguments.
 	assert.Equal(t, property.NewMap(map[string]property.Value{
-		"value":                      property.New("foo"),
-		"__pulumi-go-provider-infer": property.New(true),
+		"value": property.New("foo"),
+		"__internal": property.New(property.NewMap(map[string]property.Value{
+			"pulumi-go-provider-infer": property.New(true),
+		})),
 	}), resp.Inputs)
 }
 
@@ -61,23 +63,35 @@ func TestCheckConfigCustom(t *testing.T) {
 
 	t.Run("empty", func(t *testing.T) {
 		t.Parallel()
-		test(t, property.Map{}, property.NewMap(map[string]property.Value{
-			"__pulumi-go-provider-infer": property.New(true),
-		}))
+		test(t, property.Map{},
+			property.NewMap(map[string]property.Value{
+				"__internal": property.New(property.NewMap(map[string]property.Value{
+					"pulumi-go-provider-infer": property.New(true),
+				})),
+			}),
+		)
 	})
 	t.Run("unknown", func(t *testing.T) {
 		t.Parallel()
 		test(t,
 			property.NewMap(map[string]property.Value{"unknownField": property.New("bar")}),
-			property.NewMap(map[string]property.Value{"__pulumi-go-provider-infer": property.New(true)}))
+			property.NewMap(map[string]property.Value{
+				"__internal": property.New(property.NewMap(map[string]property.Value{
+					"pulumi-go-provider-infer": property.New(true),
+				}))},
+			),
+		)
 	})
 	t.Run("number", func(t *testing.T) {
 		t.Parallel()
 		test(t,
 			property.NewMap(map[string]property.Value{"number": property.New(42.0)}),
 			property.NewMap(map[string]property.Value{
-				"number":                     property.New(42.5),
-				"__pulumi-go-provider-infer": property.New(true),
-			}))
+				"number": property.New(42.5),
+				"__internal": property.New(property.NewMap(map[string]property.Value{
+					"pulumi-go-provider-infer": property.New(true),
+				})),
+			}),
+		)
 	})
 }

tests/config_test.go
@@ -142,8 +142,10 @@ func TestInferCheckConfigSecrets(t *testing.T) {
 	require.NoError(t, err)
 	require.Empty(t, resp.Failures)
 	assert.Equal(t, property.NewMap(map[string]property.Value{
-		"__pulumi-go-provider-infer": property.New(true),
-		"field":                      property.New("value").WithSecret(true),
+		"__internal": property.New(property.NewMap(map[string]property.Value{
+			"pulumi-go-provider-infer": property.New(true),
+		})),
+		"field": property.New("value").WithSecret(true),
 		"nested": property.New(map[string]property.Value{
 			"int":        property.New(1.0).WithSecret(true),
 			"not-nested": property.New("not-secret"),
@@ -225,10 +227,12 @@ func TestInferCustomCheckConfig(t *testing.T) {
 			require.NoError(t, err)
 			require.Empty(t, resp.Failures)
 			assert.Equal(t, property.NewMap(map[string]property.Value{
-				"__pulumi-go-provider-infer": property.New(true),
-				"field":                      property.New("value").WithSecret(true),
-				"not":                        property.New("not-secret"),
-				"applyDefaults":              property.New(applyDefaults),
+				"__internal": property.New(property.NewMap(map[string]property.Value{
+					"pulumi-go-provider-infer": property.New(true),
+				})),
+				"field":         property.New("value").WithSecret(true),
+				"not":           property.New("not-secret"),
+				"applyDefaults": property.New(applyDefaults),
 			}), resp.Inputs)
 		})
 	}

tests/grpc/config_test.go
@@ -114,8 +114,10 @@ func TestBasicConfig(t *testing.T) {
     },
     "response": {
       "inputs": {
-        "__pulumi-go-provider-infer": true,
-        "__pulumi-go-provider-version": "{{VERSION}}",
+        "__internal": {
+          "pulumi-go-provider-infer": true,
+          "pulumi-go-provider-version": "{{VERSION}}"
+        },
         "a": [
           "one",
           "two"
@@ -338,8 +340,10 @@ func TestConfigWithSecrets(t *testing.T) {
     },
     "response": {
       "inputs": {
-        "__pulumi-go-provider-infer": true,
-        "__pulumi-go-provider-version": "{{VERSION}}",
+        "__internal": {
+          "pulumi-go-provider-infer": true,
+          "pulumi-go-provider-version": "{{VERSION}}"
+        },
         "a": [
           "one",
           "two"
@@ -566,8 +570,10 @@ func TestJSONEncodedConfig(t *testing.T) {
     },
     "response": {
         "inputs": {
-            "__pulumi-go-provider-infer": true,
-            "__pulumi-go-provider-version": "{{VERSION}}",
+            "__internal": {
+              "pulumi-go-provider-infer": true,
+              "pulumi-go-provider-version": "{{VERSION}}"
+            },
             "a": [
                 "one",
                 "two"